0b0944967bba71f3e3bc51675875ad4a66dc1633
wohoman <hgy8401@naver.com>
2023-10-07 16:00:27 +0900

    짝지어 제거하기

---
+++ lv2/even_delete.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack=[]
+    
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    if not stack:
+        return 1
+    else:
+        return 0
