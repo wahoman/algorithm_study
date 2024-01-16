143d4b06e8829d177724cef45243ceff437103f1
wahoman <hgy8401@naver.com>
2024-01-16 19:11:04 +0900

    피로도

--- lv2/tired.py
+++ lv2/tired.py
@@ -1,11 +1,15 @@
+from itertools import permutations
+
 def solution(k, dungeons):
-    def explore(k,dungeons):
-        max_dungeons=0
-        for i, dungeon in enumerate(dungeons):
-            if k>=dungeon[0]:
-                remaining_dungeons = dungeons[:i] + dungeons[i+1:]
-                max_dungeons=max(max_dungeons, 1+explore(k-dungeon[1], remaining_dungeons))
-        return max_dungeons
-    
-    return explore(k,dungeons)
-                                
+    max_dungeons = 0
+    for dungeon_order in permutations(dungeons):
+        fatigue = k
+        count = 0
+        for dungeon in dungeon_order:
+            if fatigue >= dungeon[0]:
+                fatigue -= dungeon[1]
+                count += 1
+            else:
+                break
+        max_dungeons = max(max_dungeons, count)
+    return max_dungeons
